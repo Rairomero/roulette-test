ba8690ff519873c813715884855133bb94f94ae8
Rai Romero <rainieromadrid@gmail.com>
2019-06-03 15:45:31 -0400

    scaffolding player
    Signed-off-by: Rai Romero <rainieromadrid@gmail.com>

---
+++ spec/views/players/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "players/index", type: :view do
+  before(:each) do
+    assign(:players, [
+      Player.create!(
+        :name => "Name",
+        :lastname => "Lastname",
+        :balance => 2
+      ),
+      Player.create!(
+        :name => "Name",
+        :lastname => "Lastname",
+        :balance => 2
+      )
+    ])
+  end
+
+  it "renders a list of players" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Lastname".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+  end
+end
---
+++ spec/views/players/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "players/new", type: :view do
+  before(:each) do
+    assign(:player, Player.new(
+      :name => "MyString",
+      :lastname => "MyString",
+      :balance => 1
+    ))
+  end
+
+  it "renders new player form" do
+    render
+
+    assert_select "form[action=?][method=?]", players_path, "post" do
+
+      assert_select "input[name=?]", "player[name]"
+
+      assert_select "input[name=?]", "player[lastname]"
+
+      assert_select "input[name=?]", "player[balance]"
+    end
+  end
+end
